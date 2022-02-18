889ac2606c07bd2521146baef61fbe56adbbfe84
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-02-18 21:44:47 +0100

    remove button

--- todo.js
+++ todo.js
@@ -1,11 +1,19 @@
 
 
 
+// document.querySelector("#delete").addEventListener("click", createTable);
+
 function addListItem(){
 
-let input = document.querySelector("#inputItem").value;
-let listItem = document.querySelector("#todoListItem");
-let li = listItem.content.cloneNode(true);
-li.querySelector("#listItem").textContent = input;
-document.querySelector("#todoList").append(li)
+    let input = document.querySelector("#inputItem").value;
+    let listItem = document.querySelector("#todoListItem");
+    let li = listItem.content.firstElementChild.cloneNode(true);
+    li.querySelector("#listItem").textContent = input;
+    document.querySelector("#todoList").appendChild(li)
+
+    document.querySelector("#inputItem").value ='';
+
+    li.querySelector("#delete").onclick = () => {
+        li.remove();
+    }
 }
