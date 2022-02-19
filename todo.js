4a4810b0ee8c82951b2e1727a852bfdc4f7f1a20
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-02-19 13:17:05 +0100

    check all

--- todo.js
+++ todo.js
@@ -4,12 +4,24 @@ function addListItem(){
     let input = document.querySelector("#inputItem").value;
     let listItem = document.querySelector("#todoListItem");
     let li = listItem.content.firstElementChild.cloneNode(true);
-    li.querySelector("#listItem").textContent = input;
+    li.querySelector(".listItem").textContent = input;
     document.querySelector("#todoList").appendChild(li)
 
     document.querySelector("#inputItem").value ='';
 
-    li.querySelector("#delete").onclick = () => {
+    li.querySelector(".delete").onclick = () => {
         li.remove();
     }
 }
+
+
+function checkAllItems (){
+
+    let items = document.querySelectorAll(".list")
+
+    for (const e of items) {
+        e.childNodes[1].checked = true;
+    }
+}
+
+
